663b4118cfb601b39b4c5bf04807748f6d56d3a4
Jaroslaw Nowosad <yarenty@gmail.com>
2022-04-14 21:44:02 +0100

    multiplication

--- src/matrix/multiplication.rs
+++ src/matrix/multiplication.rs
@@ -4,10 +4,12 @@ pub fn rec_int_mult(x: u64, y: u64) -> u64 {
         x * y
     } else {
         let half = n / 2;
+        
         let a = x / 10_u64.pow(half);
         let b = x % 10_u64.pow(half);
         let c = y / 10_u64.pow(half);
         let d = y % 10_u64.pow(half);
+
         let ac = rec_int_mult(a, c);
         let ad = rec_int_mult(a, d);
         let bc = rec_int_mult(b, c);
@@ -39,17 +41,40 @@ pub fn karatsuba(x: u64, y: u64) -> u64 {
         let b = x % 10_u64.pow(half);
         let c = y / 10_u64.pow(half);
         let d = y % 10_u64.pow(half);
-        
+
         let p = a + b;
         let q = c + d;
-        
+
         let ac = karatsuba(a, c);
         let bd = karatsuba(b, d);
-
         let pq = karatsuba(p, q);
 
         let abcd = pq - ac - bd;
 
         10_u64.pow(n) * ac + 10_u64.pow(half) * abcd + bd
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_karatsuba() {
+        let x = 1234;
+        let y = 5678;
+        let expected = x * y;
+        let result = karatsuba(x, y);
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_rec_int_mult() {
+        let x = 1234;
+        let y = 5678;
+        let expected = x * y;
+        let result = rec_int_mult(x, y);
+        assert_eq!(result, expected);
     }
 }
